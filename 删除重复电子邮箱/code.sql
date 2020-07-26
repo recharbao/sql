382b9d7d4d8b23cd21ef527cfaaf5101ee7db714
recharbao <884520248@qq.com>
2020-07-26 12:16:10 +0800

    sql

---
+++ 删除重复电子邮箱/code.sql
@@ -0,0 +1,38 @@
+﻿use combinetowtable;
+
+create table PersonEmail(
+  id int primary key auto_increment,
+  Email varchar(100)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+insert into PersonEmail values(0,'john@example.com');
+insert into PersonEmail values(0,'bao@example.com');
+insert into PersonEmail values(0,'li@example.com');
+insert into PersonEmail values(0,'wang@example.com');
+insert into PersonEmail values(0,'huang@example.com');
+insert into PersonEmail values(0,'fei@example.com');
+insert into PersonEmail values(0,'lv@example.com');
+insert into PersonEmail values(0,'zhang@example.com');
+insert into PersonEmail values(0,'zhao@example.com');
+insert into PersonEmail values(0,'fei@example.com');
+insert into PersonEmail values(0,'fei@example.com');
+insert into PersonEmail values(0,'fei@example.com');
+insert into PersonEmail values(0,'fei@example.com');
+insert into PersonEmail values(0,'fei@example.com');
+insert into PersonEmail values(0,'fei@example.com');
+insert into PersonEmail values(0,'fei@example.com');
+insert into PersonEmail values(0,'fei@example.com');
+insert into PersonEmail values(0,'fei@example.com');
+insert into PersonEmail values(0,'fei@example.com');
+
+select * from PersonEmail;
+
+delete p1 from PersonEmail p1,
+PersonEmail p2 where p1.Email = p2.Email and p1.id > p2.id;
+
+
+
+
+
---
+++ 第n高薪水/code.sql
@@ -0,0 +1,32 @@
+﻿use combineTowTable;
+
+create table Employee(
+  Id int primary key auto_increment,
+  Salary int
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+insert into Employee values(0,100);
+insert into Employee values(0,200);
+insert into Employee values(0,300);
+insert into Employee values(0,400);
+insert into Employee values(0,400);
+insert into Employee values(0,500);
+insert into Employee values(0,600);
+
+select * from Employee;
+
+
+-- 查找第n高薪水
+
+select distinct Salary from Employee
+order by Salary desc limit 1 offset 3;
+
+select Salary from Employee
+order by Salary desc limit 1 offset 3;
+
+
+
+
+
+
+
